b9276c7a43de0f5da7c0dd16b13a9ef8f136ae24
juliohiguita <158359335+juliohiguita@users.noreply.github.com>
2024-02-03 15:23:43 -0500

    version 1

---
+++ app.js
@@ -0,0 +1,89 @@
+let areaEncriptar = document.getElementById("areaEncriptar");
+
+const btnEncriptar = document.querySelector(".btn__encriptar");
+const btnDesencriptar = document.querySelector(".btn__desencriptar");
+
+const elements = document.querySelectorAll(".elements");
+
+const btnCopiar = document.querySelector(".boton__aside");
+const txtcopiado = document.querySelector(".txtcopiado");
+
+let textEncriptado = document.querySelector(".tex_encriptado");
+
+// ocultamos los elementos no deseados
+const ocultarElementos = () => {
+  elements.forEach((elemento) => elemento.classList.add("ocultar"));
+  btnCopiar.classList.remove("ocultar");
+  textEncriptado.classList.remove("ocultar");
+};
+
+// bloque, encriptar
+const encriptar = () => {
+  let texto = areaEncriptar.value;
+  texto = texto
+    .replaceAll(/e/gi, "enter")
+    .replaceAll(/i/gi, "imes")
+    .replaceAll(/o/gi, "ober")
+    .replaceAll(/a/gi, "ai")
+    .replaceAll(/u/gi, "ufat");
+  textEncriptado.value = texto;
+  areaEncriptar.value = "";
+};
+
+// bloque, desencriptar
+const desenCriptar = () => {
+  let texto = areaEncriptar.value;
+  texto = texto
+    .replaceAll(/enter/gi, "e")
+    .replaceAll(/imes/gi, "i")
+    .replaceAll(/ober/gi, "o")
+    .replaceAll(/ai/gi, "a")
+    .replaceAll(/ufat/gi, "u");
+  textEncriptado.value = texto;
+  areaEncriptar.value = "";
+};
+
+//comprobamos los campos vacíos
+const comprobarVacios = () => {
+  if (areaEncriptar.value.trim() == "") {
+    alert("ingresa una palabra");
+    location.reload();
+  }
+};
+
+// detectamos si tiene activado el BloqMayus
+areaEncriptar.addEventListener("keyup", (event) => {
+  if (event.getModifierState("CapsLock")) {
+    alert("Solo se puede escribir en minúscula");
+    location.reload();
+  }
+});
+
+// encriptamos
+btnEncriptar.addEventListener("click", () => {
+  comprobarVacios();
+  ocultarElementos();
+  encriptar();
+});
+
+// desencriptamos
+btnDesencriptar.addEventListener("click", () => {
+  comprobarVacios();
+  ocultarElementos();
+  desenCriptar();
+});
+
+// btn copiar
+btnCopiar.addEventListener("click", () => {
+  // accedemos a los valores
+  textEncriptado.select();
+  textEncriptado.setSelectionRange(0, 99999);
+
+  navigator.clipboard.writeText(textEncriptado.value);
+
+  
+  txtcopiado.classList.remove("ocultar");
+  setTimeout(() => {
+    txtcopiado.classList.add("ocultar");
+  }, 1000);
+});
